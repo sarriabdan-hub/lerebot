8587e2bef648b8654eaa532f809ba066f1b837ce
Sari Abdan <sari.abdan@tu-clausthal.de>
2026-03-19 05:07:36 +0000

    Edit Main.javafpfiewj

--- src/main/java/tuc/isse/Main.java
+++ src/main/java/tuc/isse/Main.java
@@ -2,6 +2,6 @@ package tuc.isse;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worldy");
     }
 }
